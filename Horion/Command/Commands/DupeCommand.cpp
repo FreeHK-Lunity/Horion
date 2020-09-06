226164f588da03cf4e3d4a9325ad25b1f033ceb3
FreeHK-Lunity <67418738+FreeHK-Lunity@users.noreply.github.com>
2020-09-06 16:13:57 +0800

    Remove nukkit dupe(dunno if it will work

--- Horion/Command/Commands/DupeCommand.cpp
+++ Horion/Command/Commands/DupeCommand.cpp
@@ -1,6 +1,7 @@
 #include "DupeCommand.h"
 
 DupeCommand::DupeCommand() : IMCCommand("dupe", "Duplicates the item in hand", "") {
+DupeCommand::DupeCommand() : IMCCommand("dupe", "Duplicates the item in hand", "<count> <mode: give / offhand : 1/0>") {
 }
 
 DupeCommand::~DupeCommand() {
@@ -10,6 +11,8 @@ bool DupeCommand::execute(std::vector<std::string>* args) {
 	C_LocalPlayer* player = g_Data.getLocalPlayer();
 
 	auto supplies = g_Data.getLocalPlayer()->getSupplies();
+	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
+	C_Inventory* inv = supplies->inventory;
 	auto transactionManager = g_Data.getLocalPlayer()->getTransactionManager();
 
 	C_ItemStack* selectedItem = g_Data.getLocalPlayer()->getSelectedItem();
@@ -30,6 +33,17 @@ bool DupeCommand::execute(std::vector<std::string>* args) {
 
 			numActions += 2;
 		}
+	int selectedSlot = supplies->selectedHotbarSlot;
+	C_ItemStack* item = inv->getItemStack(selectedSlot);
+
+	int count = item->count;
+	bool isGive = true;
+	if (args->size() > 1)
+		item->count = assertInt(args->at(1));
+	if (args->size() > 2)
+		isGive = static_cast<bool>(assertInt(args->at(2)));
+	if (isGive) {
+		int slot = inv->getFirstEmptySlot();
 	}
 
 	C_InventoryTransactionPacket pk;
@@ -55,5 +69,18 @@ bool DupeCommand::execute(std::vector<std::string>* args) {
 	pk.numTransactions = 1;
 	g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&pk);
 	clientMessageF("%sSuccessfully duplicated item! (%i times)", GREEN, numActions / 2);
+		C_InventoryAction* firstAction = nullptr;
+		C_InventoryAction* secondAction = nullptr;
+		firstAction = new C_InventoryAction(0, item, nullptr, 507, 99999);
+		secondAction = new C_InventoryAction(slot, nullptr, item);
+
+		transactionManager->addInventoryAction(*firstAction);
+		transactionManager->addInventoryAction(*secondAction);
+		inv->addItemToFirstEmptySlot(item);
+	} else
+		g_Data.getLocalPlayer()->setOffhandSlot(item);
+	if (args->size() > 1)
+		item->count = count;
+	clientMessageF("%sSuccessfully duplicated the item!", GREEN);
 	return true;
 }
